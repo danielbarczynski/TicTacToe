cbe9fc040e817731bf58bd13bf2317bc005be0f8
WhatIveBecome <daniel.barczynski@onet.pl>
2021-10-16 18:55:11 +0200

    Added users, classes and turns

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,6 +7,19 @@ namespace TicTacToe
         static void Main(string[] args)
         {
 
+            // Create two players
+
+            HumanPlayer p1 = new HumanPlayer();
+            ComputerPlayer p2 = new ComputerPlayer();
+
+            p1.Name = "User";
+            p1.Symbol = 'X';
+
+            p2.Name = "AI";
+            p2.Symbol = 'O';
+
+            // Create two boards - with starting and current fields
+
             char[,] startBoard = {
             { '1', '2', '3' }, // only i row
             { '4', '5', '6' }, // now another row in j, so there it is [,]
@@ -29,12 +42,14 @@ namespace TicTacToe
 
                 if (player1Move)
                 {
-                    // TODO: player 1 move
+                    Console.WriteLine(p1.Name + " turn");
+                    gameEnded = p1.MakeMove(startBoard, gameBoard);
                     player1Move = false; // after completed move, changing to player2
                 }
                 else
                 {
-                    // TODO: player 2 move
+                    Console.WriteLine(p2.Name + " turn");
+                    gameEnded = p2.MakeMove(startBoard, gameBoard);
                     player1Move = true; // after completed move, changing to player1 - back to if (player1Move)
                 }
                 if (gameEnded)
@@ -57,75 +72,94 @@ namespace TicTacToe
                 for (int j = 0; j < board.GetLength(1); j++) // for width
                     Console.Write(board[i, j]); // get element from the [i,j] - width and lenght
                 Console.WriteLine(); // print whole row [i,j] then jump to another line
-                    
+
                 // if "writeline" every element in other line, I want whole row
             }
         }
+    }
+    interface IMoving // interface starts with "I" on the beginning
+    {
+        bool MakeMove(char[,] startBoard, char[,] gameBoard);
+    }
 
-        interface IMoving
+    abstract class Player // abstract means that we cannot make object to this class, share part for AI and player
+    {
+        public string Name { get; set; }
+        public char Symbol { get; set; } // X or O
+
+        public bool CheckIfPlayerWon (char[,] gameBoard) // checking for XXX or OOO in one row/line/diagonally
         {
-            bool Makemove(char[,] startBoard, char[,] gameBoard);
-        }
+            int height = gameBoard.GetLength(0);
+            int width = gameBoard.GetLength(1); // to save some time, two variables
+            if (height != width)
+                throw new Exception("The board is not a square!"); // our board is a square, it is just protection if whatever happens
 
-        abstract class Player // abstract means that we cannot make object to this class, share part for AI and player
-        {
-            public string Name { get; set; }
-            public char Symbol { get; set; } // X or O
+            // Check rows
 
-            public bool CheckIfPlayerWon (char[,] gameBoard) // checking for XXX or OOO in one row/line/diagonally
+            for (int i = 0; i < height; i++)
             {
-                int height = gameBoard.GetLength(0);
-                int width = gameBoard.GetLength(1); // to save some time, two variables
-                if (height != width)
-                    throw new Exception("The board is not a square!"); // our board is a square, it is just protection if whatever happens
-
-                // Check rows
-
-                for (int i = 0; i < height; i++)
+                int rowSum = 0; // starting from 0
+                for (int j = 0; j < width; j++)
                 {
-                    int rowSum = 0; // starting from 0
-                    for (int j = 0; j < width; j++)
-                    {
-                        if (gameBoard[i, j] == Symbol)
-                            rowSum++;
-                    }
-                    if (rowSum == width) // could also say 3, but we stay to width/height just like in i < board.GetLength 
-                        return true; // return finish the method, executes only rowSum == width
+                    if (gameBoard[i, j] == Symbol)
+                        rowSum++;
                 }
-
-                // Check colums
-
-                for (int j = 0; j < width; j++) // starting with j height because of checkmate in columns
-                {
-                    int colSum = 0;
-                    for (int i = 0; i < height; i++)
-                    {
-                        if (gameBoard[i, j] == Symbol)
-                            colSum++;
-                    }
-                    if (colSum == height)
-                        return true;
-                }
-
-                // Check diagonals
-
-                int diagSumA = 0;
-                int diagSumB = 0;
-                for (int k = 0; k < width; k++)
-                {
-                    if (gameBoard[k, k] == Symbol) // for [0,0] [1,1] [2,2]
-                        diagSumA++;
-                    if (gameBoard[k, width - 1 - k] == Symbol) // for [0,2] [1,1] [2,0] 
-                        diagSumB++; // numbers on table are 0,1,2 width is 3 so: width - 1 and also k \\\ e.g k=0 [k, 2-k] 
-                }
-                if (diagSumA == width || diagSumB == width) // || means or
-                    return true;
-
-                // otherwise, not win yet
-
-                return false;
+                if (rowSum == width) // could also say 3, but we stay to width/height just like in i < board.GetLength 
+                    return true; // return finish the method, executes only rowSum == width
             }
 
+            // Check colums
+
+            for (int j = 0; j < width; j++) // starting with j height because of checkmate in columns
+            {
+                int colSum = 0;
+                for (int i = 0; i < height; i++)
+                {
+                    if (gameBoard[i, j] == Symbol)
+                        colSum++;
+                }
+                if (colSum == height)
+                    return true;
+            }
+
+            // Check diagonals
+
+            int diagSumA = 0;
+            int diagSumB = 0;
+            for (int k = 0; k < width; k++)
+            {
+                if (gameBoard[k, k] == Symbol) // for [0,0] [1,1] [2,2]
+                    diagSumA++;
+                if (gameBoard[k, width - 1 - k] == Symbol) // for [0,2] [1,1] [2,0] 
+                    diagSumB++; // numbers on table are 0,1,2 width is 3 so: width - 1 and also k \\\ e.g k=0 [k, 2-k] 
+            }
+            if (diagSumA == width || diagSumB == width) // || means or
+                return true;
+
+            // otherwise, not win yet
+
+            return false;
+        }
+    }
+
+    class HumanPlayer : Player, IMoving // inherit after class Player and implement interface IMoving 
+    {
+        public bool MakeMove(char[,] startBoard, char[,] gameBoard)
+        {
+            // TODO: human move         
+            bool result = CheckIfPlayerWon(gameBoard);
+            return result;
+        }
+    }
+    class ComputerPlayer : Player, IMoving // inherit after class Player and implement interface IMoving 
+    {
+        public bool MakeMove(char[,] startBoard, char[,] gameBoard)
+        {
+            // TODO: computer move
+
+            bool result = CheckIfPlayerWon(gameBoard);
+            return result;
         }
     }
 }
+
